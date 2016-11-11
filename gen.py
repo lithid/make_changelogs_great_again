2ccd062873b87874427cecb0d3ed1986778348dd
Jeremie Long <jeremie.long@mobiledefense.com>
2016-11-11 16:29:22 -0500

    Update python path
    #Fixed fixed the python path for macos

--- gen.py
+++ gen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/python
 import sys
 import subprocess
 import time
@@ -7,7 +7,7 @@ NUMBER_ARGS = 5
 
 class Gen:
 
-    def main(self, kind, commit, version, build):
+    def main(self, kind, commit, version, build, show_author):
         sections = {}
 
         command_value = ""
@@ -20,8 +20,13 @@ class Gen:
             command_value = "%s..%s" % (item_split_alpha, item_split_bravo)
 
         output = subprocess.check_output(["git", "log", command_value])
+        author = ""
         for x in output.splitlines():
             value = x.strip()
+
+            if value.startswith("Author:"):
+                author = "(%s)" % value.replace("Author:", "").strip()
+
             if value.startswith("#"):
                 name = value.split(" ")[0]
                 content = value.replace(name, "").strip()
@@ -30,7 +35,11 @@ class Gen:
                     sections[name] = []
 
                 bucket = sections[name]
-                bucket.append(content)
+                if show_author:
+                    bucket.append("%s %s" % (content, author))
+                else:
+                    bucket.append(content)
+
                 sections[name] = bucket
 
         print "## %s (%s) - %s" % (version, build, time.strftime("%d-%m-%Y"))
@@ -42,15 +51,20 @@ class Gen:
 
 if __name__ == "__main__":
     number_of_args = len(sys.argv)
-    if number_of_args != NUMBER_ARGS:
+    if number_of_args < NUMBER_ARGS:
         arg_string = "argument"
         if number_of_args != 2:
             arg_string = "arguments"
         print "Invalid arguments, %d %s won't cut it" % ((number_of_args - 1), arg_string)
         print "-c   --commit        Use commit"
         print "-t   --tags          Use tags"
+        print "-a   --author        Show author on commits"
         print "Example: gen.py -c 06141946 0.0.1 001"
         print "Example: gen.py -t v0.0.1,v0.0.2 0.0.2 002"
     else:
         G = Gen()
-        G.main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        if len(sys.argv) == 6:
+            if sys.argv[5] == "-a" or sys.argv[5] == "--author":
+                G.main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], True)
+        else:
+            G.main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], False)
