e3b8dac3373dacf9f0c2b305732b47673aa0e1e5
Jeremie Long <jeremie.long@mobiledefense.com>
2016-11-11 14:51:12 -0500

    Print the changelog
    #Update updated script to print out to system

--- gen.py
+++ gen.py
@@ -7,12 +7,28 @@ NUMBER_ARGS = 2
 class Gen:
 
     def main(self, commit):
+        sections = {}
         output = subprocess.check_output(["git", "log", "%s^..HEAD" % commit])
         output = output.splitlines()
         for x in output:
-            x = x.strip()
-            if x.startswith("#"):
-                print x
+            value = x.strip()
+            if value.startswith("#"):
+                name = value.split(" ")[0]
+                content = value.replace(name, "").strip()
+
+                if name not in sections:
+                    sections[name] = []
+
+                bucket = sections[name]
+                bucket.append(content)
+                sections[name] = bucket
+
+        print "Changelog:\n"
+        for something in sections:
+            print "### %s" % something.replace("#", "")
+            for another in sections[something]:
+                print "- %s" % another
+            print ""
 
 if __name__ == "__main__":
     if len(sys.argv) != NUMBER_ARGS:
